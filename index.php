8b9c5adc5b344d6350f64b67e47a0fb31d86f18a
Elric Barkey <ebarkey2@mail.greenriver.edu>
2020-05-20 17:32:01 -0400

    added route to survey in index.php, created a model that contains my function that returns my array of options, added inputs to survey

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ session_start();
 
 //require the auto load file
 require_once ("vendor/autoload.php");
+require_once ("model/data-layer.php");
 
 //instantiate the F3 Base class
 $f3 = Base::instance();
@@ -17,11 +18,31 @@ $f3 = Base::instance();
 $f3->route('GET /', function()
 {
     echo "<h1>Midterm Survey</h1>";
-    echo "<a href= '#' >Take my Midterm Survey</a>";
-    //$view = new Template();
-    //echo $view->render('views/home.html');
+    echo "<a href= 'survey' >Take my Midterm Survey</a>";
+
 });
 
+//survey route
+$f3->route('GET /survey', function($f3)
+{
+    $options = getOptions();
+
+    //if the form has been submitted
+    if($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        //store the data in the session array
+        $_SESSION['options'] = $_POST['options'];
+
+        $f3->reroute('summary');
+        session_destroy();
+
+    }
+
+    $f3->set('options', $options);
+
+    $view = new Template();
+    echo $view->render('views/survey.html');
+});
 
 
 //run f3
---
+++ model/data-layer.php
@@ -0,0 +1,14 @@
+<?php
+
+    /* get survey options
+       * return array of survey options
+       * @return array
+     *
+     */
+
+    function getOptions()
+    {
+        $options = array("This midterm is easy", "I like midterms", "Today is Friday");
+        return $options;
+    }
+
